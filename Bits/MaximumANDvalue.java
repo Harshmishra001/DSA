780853b0d863c38bfb261284c5ffc9c6a5b547bb
Harshmishra001 <iconharsh01@gmail.com>
2024-09-10 17:00:28 +0530

    DSA

--- Bits/MaximumANDvalue.java
+++ Bits/MaximumANDvalue.java
@@ -41,3 +41,39 @@ public class MaximumANDvalue {
     }
 }
 
+// MEANING OF QN.
+// Execution for the Given Input:
+// Array: {4, 8, 12, 16}
+
+// Possible Pairs and Their AND Values:
+// (4, 8):
+
+// 4 & 8 = 00000100 & 00001000 = 00000000
+// Result: 0
+
+// (4, 12):
+
+// 4 & 12 = 00000100 & 00001100 = 00000100
+// Result: 4
+
+// (4, 16):
+
+// 4 & 16 = 00000100 & 00010000 = 00000000
+// Result: 0
+
+// (8, 12):
+
+// 8 & 12 = 00001000 & 00001100 = 00001000
+// Result: 8   , YHI HAI HIGHEST 8 
+
+// (8, 16):
+
+// 8 & 16 = 00001000 & 00010000 = 00000000
+// Result: 0
+// (12, 16):
+// 12 & 16 = 00001100 & 00010000 = 00000000
+// Result: 0
+
+// Maximum AND Value:
+
+// From the results above, the highest AND value is 8, which comes from the pair (8, 12).
